d99fa6f8f1d5f5bde3c29fde63a992e39894b6a5
mvrichard825 <mvrichard825@gmail.com>
2025-07-14 12:30:28 +0200

    added

---
+++ index.js
@@ -0,0 +1,13 @@
+const name1="Richard";
+let age =30;
+var isEmployed=true;
+console.log("am I employed?"+isEmployed);
+console.log("my Name:"+name1);
+console.log("my Age:"+age);
+const a=10;
+const b=50;
+// operators
+console.log("Addition:",a <= b);
+if(age >=18){
+    console.log("you can vote");
+}
